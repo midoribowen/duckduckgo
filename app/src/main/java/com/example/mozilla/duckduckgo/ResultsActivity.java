39108de7fa62c43dc7af773b2d7ad60546998191
Midori Bowen <midori.p.bowen@gmail.com>
2018-08-04 21:21:25 -0700

    move webview load operations to onCreate to mainstain state onPause

--- app/src/main/java/com/example/mozilla/duckduckgo/ResultsActivity.java
+++ app/src/main/java/com/example/mozilla/duckduckgo/ResultsActivity.java
@@ -1,7 +1,6 @@
 package com.example.mozilla.duckduckgo;
 
 import android.content.Intent;
-import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.webkit.WebView;
@@ -19,19 +18,8 @@ public class ResultsActivity extends AppCompatActivity {
         mWebView.getSettings().setJavaScriptEnabled(true);
         Intent intent = getIntent();
         mTerm = intent.getStringExtra(SEARCH_TERM);
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
         String url = getString(R.string.duckduckgo_url);
         url += mTerm;
         mWebView.loadUrl(url);
-    }
-
-    // this is here to prevent webview from reloading on screen rotation
-    @Override
-    public void onConfigurationChanged(Configuration newConfig) {
-        super.onConfigurationChanged(newConfig);
     }
 }
